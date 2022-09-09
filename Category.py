7222d60f475e853d17b43aa6a64e0088854a6dba
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2022-09-09 19:50:28 +0200

    Category.py     adding the public method writeCSV()     adding the private method getHeaders(), to make easier the csv writing     adding th method print() to make easier the debug

--- Category.py
+++ Category.py
@@ -1,4 +1,7 @@
+from fileinput import filename
+from Book import Book
 from Page import Page
+import csv
 
 """ Headers :
 
@@ -23,6 +26,21 @@ class Category(Page):
         self.__category = category
         self.__booksURL = []
         self.__readBookURLs()
+
+    def writeCSV(cls, fileLocation):
+
+        fileName = fileLocation + cls.__category + ".csv"
+
+        fichier = open(fileName, "wb")
+        fichier.write(str(cls.__getHeaders()).encode('utf8'))
+
+        for urlBook in cls.__booksURL:
+            book = Book(urlBook, cls.__category)
+            fichier.write(book.toString())
+
+        fichier.close()
+
+        print("printing csv : '" + cls.__category + "' done.")
 
     def __readBookURLs(cls):
 
@@ -54,4 +72,35 @@ class Category(Page):
 
     def getBookURLs(cls):
         return cls.__booksURL
+
+    def __getHeaders(cls):
+
+        headers = [None] * 10
+
+        headers[0] = "product_page_url"
+        headers[1] = "universal_ product_code (upc)"
+        headers[2] = "title"
+        headers[3] = "price_including_tax"
+        headers[4] = "price_excluding_tax"
+        headers[5] = "number_available"
+        headers[6] = "product_description"
+        headers[7] = "category"
+        headers[8] = "review_rating"
+        headers[9] = "image_url"
+
+        return ";".join(headers) + "\n"
+
+    def print(cls):
+
+        print(cls.__category, ": nb Results =", len(cls.__booksURL))
+        index = 0
+
+        for urlBook in cls.__booksURL:
+            index += 1
+            #book = Book(urlBook, cls.__category)
+            #print("     " + "%02d" % index, book.getTitle())
+
+            print("     " + "%02d" % index, urlBook)
+        
+        print(" ")            
     
